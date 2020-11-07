e7acdfc506c125f9afe8660622e7a367d1872879
Jacky20200711 <fewer135+1@gmail.com>
2020-11-07 20:50:43 +0800

    呈現工作項目之前，先檢查是否經過解析 & 解析過後的結果

--- JobFilter/Controllers/JobFilterController.cs
+++ JobFilter/Controllers/JobFilterController.cs
@@ -1,6 +1,7 @@
 ﻿using JobFilter.Data;
 using JobFilter.Models.DataStructure;
 using JobFilter.Models.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -12,6 +13,7 @@ using X.PagedList;
 
 namespace JobFilter.Controllers
 {
+    [Authorize]
     public class JobFilterController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -25,7 +27,18 @@ namespace JobFilter.Controllers
         {
             page = page == null ? 1 : page;
 
-            return View(await JsonConvert.DeserializeObject<Jobs>(HttpContext.Session.GetString("Jobs")).ToPagedListAsync((int)page, 10));
+            if(HttpContext.Session.GetString("Jobs") == null)
+            {
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+
+            var jobs = JsonConvert.DeserializeObject<Jobs>(HttpContext.Session.GetString("Jobs"));
+            if(jobs.Count == 0)
+            {
+                TempData["NoMatchingJob"] = "查無符合的工作!";
+            }
+
+            return View(await jobs.ToPagedListAsync((int)page, 10));
         }
 
         public IActionResult DoCrawl(int? id)
@@ -43,21 +56,27 @@ namespace JobFilter.Controllers
             }
             string TargetUrl = filterSetting.CrawlUrl;
 
+            // 在目標網址尾端添加指定頁數
+            char ConnectionChar = TargetUrl.Last() == '/' ? '?' : '&';
+
             // 創建多個爬蟲
             List<JobCrawler> JobCrawlers = new List<JobCrawler>
             {
-                new JobCrawler($"{TargetUrl}&page=1"),
-                new JobCrawler($"{TargetUrl}&page=2"),
+                new JobCrawler($"{TargetUrl}{ConnectionChar}page=1"),
+                new JobCrawler($"{TargetUrl}{ConnectionChar}page=2"),
+                new JobCrawler($"{TargetUrl}{ConnectionChar}page=3"),
             };
 
             // 創建爬蟲容器 & 各自對應到一個 Thread (令爬蟲就位XD)
             JobFilterThread JobFilterThread0 = new JobFilterThread(JobCrawlers[0]);
             JobFilterThread JobFilterThread1 = new JobFilterThread(JobCrawlers[1]);
+            JobFilterThread JobFilterThread2 = new JobFilterThread(JobCrawlers[2]);
 
             List<Thread> JobFilterThreads = new List<Thread>
             {
                 new Thread(JobFilterThread0.DoFilter),
                 new Thread(JobFilterThread1.DoFilter),
+                new Thread(JobFilterThread2.DoFilter),
             };
 
             // 執行所有的 Thread
@@ -67,7 +86,10 @@ namespace JobFilter.Controllers
             }
 
             // 等待所有 Thread 完成任務
-            while (!JobCrawlers.All(jobCrawler => jobCrawler.IsMissionComplete() == true)) ;
+            while (!JobCrawlers.All(jobCrawler => jobCrawler.IsMissionComplete() == true))
+            {
+                Thread.Sleep(500);
+            }
 
             // 過濾掉不符合條件的工作
             Jobs jobs = new Jobs();
