1169702d8199bde292c6007be75794e42a00338f
Jacky20200711 <fewer135+1@gmail.com>
2021-01-11 19:47:55 +0800

    為用戶管理的建立功能添加單元測試

---
+++ JobFilterTests/Models/Services/IdentityUserManagerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobFilter.Models.Services.Tests
+{
+    [TestClass()]
+    public class IdentityUserManagerTests
+    {
+        [TestMethod()]
+        public void IsValidUserTest()
+        {
+            string ErrorMessage = "輸入資料錯誤!";
+            IdentityUser identityUser = new IdentityUser();
+
+            // 測試郵件檢測
+            identityUser.PasswordHash = "123456";
+            identityUser.Email = null;
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.Email = "";
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.Email = "123456";
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.Email = "@gmail.com";
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.Email = "123456@gmail.com";
+            Assert.AreEqual(null, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.Email = "123456@yahoo.com.tw";
+            Assert.AreEqual(null, IdentityUserManager.IsValidUser(identityUser));
+
+            // 測試密碼檢測
+            identityUser.Email = "123456@yahoo.com.tw";
+            identityUser.PasswordHash = null;
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.PasswordHash = "";
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.PasswordHash = "12345";
+            Assert.AreEqual(ErrorMessage, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.PasswordHash = "123456";
+            Assert.AreEqual(null, IdentityUserManager.IsValidUser(identityUser));
+
+            identityUser.PasswordHash = "X123456@";
+            Assert.AreEqual(null, IdentityUserManager.IsValidUser(identityUser));
+        }
+    }
+}
