3d89263bf6a99a3f6d524c525e193196f9f3abcd
Jacky20200711 <fewer135+1@gmail.com>
2021-01-12 19:45:13 +0800

    refactor & 修改Google的登入按鈕

---
+++ JobFilter/wwwroot/js/InputHelper.js
@@ -0,0 +1,14 @@
+﻿function AddGmail() {
+    var InputOfEmail = document.getElementById("InputOfEmail");
+    // 若 Input 沒有 gmail 則幫忙補上
+    if (InputOfEmail.value.indexOf("@@gmail") < 0) {
+        InputOfEmail.value = InputOfEmail.value + "@@gmail.com";
+    }
+}
+function AddYahoo() {
+    var InputOfEmail = document.getElementById("InputOfEmail");
+    // 若 Input 沒有 yahoo 則幫忙補上
+    if (InputOfEmail.value.indexOf("@@yahoo") < 0) {
+        InputOfEmail.value = InputOfEmail.value + "@@yahoo.com.tw";
+    }
+}
