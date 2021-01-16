e06d288a49991f55e81290f998af3a443414a466
Jacky20200711 <fewer135+1@gmail.com>
2021-01-16 11:18:03 +0800

    調整設定檔的錯誤訊息和排版

--- JobFilter/Models/DataStructure/FilterSetting.cs
+++ JobFilter/Models/DataStructure/FilterSetting.cs
@@ -7,24 +7,24 @@ namespace JobFilter.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "此欄位不能為空")]
-        [StringLength(800, ErrorMessage = "最多只能輸入800個字元!")]
+        [StringLength(800, ErrorMessage = "最多800個字")]
         public string CrawlUrl { get; set; }
 
         [Required(ErrorMessage = "此欄位不能為空")]
-        [RegularExpression(@"^[0-9''-'\d]{3,6}$", ErrorMessage = "輸入內容必須為3~6個數字")]
+        [RegularExpression(@"^[0-9''-'\d]{3,6}$", ErrorMessage = "必須為3~6個數字")]
         public int MinimumWage { get; set; }
 
         [Required(ErrorMessage = "此欄位不能為空")]
-        [RegularExpression(@"^[0-9''-'\d]{3,7}$", ErrorMessage = "輸入內容必須為3~7個數字")]
+        [RegularExpression(@"^[0-9''-'\d]{3,7}$", ErrorMessage = "必須為3~7個數字")]
         public int MaximumWage { get; set; }
 
-        [MaxLength(50, ErrorMessage = "最多只能輸入50個字元!")]
+        [MaxLength(50, ErrorMessage = "最多50個字")]
         public string ExcludeWord { get; set; }
 
-        [MaxLength(1000, ErrorMessage = "最多只能輸入1000個字元!")]
+        [MaxLength(1000, ErrorMessage = "最多1000個字")]
         public string IgnoreCompany { get; set; }
 
-        [MaxLength(5, ErrorMessage = "最多只能輸入5個字元!")]
+        [MaxLength(5, ErrorMessage = "最多5個字")]
         public string Remarks { get; set; }
 
         public string UserId { get; set; }
